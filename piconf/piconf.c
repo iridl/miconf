28763f8c077c1822fd0956535320596a6ebeb2c5
Igor Khomyakov <ikh@iri.columbia.edu>
2018-04-01 04:56:07 -0400

    started piconf

---
+++ piconf/piconf.c
@@ -0,0 +1,525 @@
+/*
+ * Copyright (c) 2012 ikh software, inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1) Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * 2) Redistributions of source code with modification must include a notice
+ * that the code was modified.
+ * 3) Neither the names of the authors nor the names of their contributors may
+ * be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <string.h>
+#include <sys/stat.h>
+#include "lua.h"
+#include "lauxlib.h"
+#include "lualib.h"
+
+#include "version.h"
+
+#define WRITE_BEG "[=====["
+#define WRITE_END "]=====]"
+
+#define NL '\n'
+
+#define doNewLine(c) fprintf(fo,"io.write('\\n') -- %d\n",lineno)
+
+#define begText() fprintf(fo,"io.write(%s",WRITE_BEG)
+#define doText(c) fputc(c,fo)
+#define endText() fprintf(fo,"%s) -- %d\n",WRITE_END,lineno)
+
+#define begStat() fputc(NL,fo)
+#define doStat(c) fputc(c,fo)
+#define endStat() fprintf(fo," -- %d\n",lineno)
+
+#define begExpr() fprintf(fo,"io.write(tostring(")
+#define doExpr(c) fputc(c,fo)
+#define endExpr() fprintf(fo,")) -- %d\n", lineno)
+
+void convert(FILE* fi, FILE* fo, int n, int eq, int la, int ra) {
+   int lineno = 1;
+   int c;
+   int s = 1; //state
+   do {
+      c=getc(fi);
+      switch (s) {
+      case 1:
+         switch (c) {
+         case EOF: s=1; break;
+         case NL: s=1; doNewLine(); break;
+         default: 
+            if (c==eq) {
+               s=2;
+            } else if(c==la) {
+               s=6; begText();
+            } else {
+               s=5; begText(); doText(c); 
+            }
+            break;
+         }
+         break;
+      case 2:
+         switch (c) {
+         case EOF: s=5; begText(); doText(eq); endText(); break;
+         case NL: s=1; begText(); doText(eq); endText(); doNewLine(); break;
+         default: 
+            if (c==eq) {
+               s=3; 
+            } else {
+               s=5; begText(); doText(eq); doText(c);
+            }
+            break;
+         }
+         break;
+      case 3:
+         switch (c) {
+         case EOF: s=5; begText(); doText(eq); doText(eq); endText(); break;
+         case NL: s=1; begText(); doText(eq); doText(eq); endText(); doNewLine(); break;
+         default: 
+            if (c==eq) {
+               s=4; begStat();
+            } else {
+               s=5; begText(); doText(eq); doText(eq); doText(c);
+            }
+            break;
+         }
+         break;
+      case 4:
+         switch (c) {
+         case EOF: s=1; endStat(); break;
+         case NL: s=1; endStat(); break;
+         default: s=4; doStat(c); break;
+         }
+         break;
+      case 5:
+         switch (c) {
+         case NL: s=1; endText(); doNewLine(); break;
+         case EOF: s=5; endText(); break;
+         default: 
+            if (c==la) {
+               s=6;
+            } else {
+               s=5; doText(c);
+            }
+            break;
+         }
+         break;
+      case 6:
+         switch (c) {
+         case EOF: s=5; doText(la); endText(); break;
+         case NL: s=1; doText(la); endText(); doNewLine(); break;
+         default: 
+            if (c==la) {
+               s=7; 
+            } else {
+               s=5; doText(la); doText(c);
+            }
+            break;
+         }
+         break;
+      case 7:
+         switch (c) {
+         case EOF: s=5; doText(la); doText(la); endText(); break;
+         case NL: s=1; doText(la); doText(la); endText(); doNewLine(); break;
+         default: 
+            if (c==la) {
+               s=8; endText(); begExpr();
+            } else {
+               s=5; doText(la); doText(la); doText(c);
+            }
+            break;
+         }
+         break;
+      case 8:
+         switch (c) {
+         case EOF: s=8; endExpr(); break;
+         default: 
+            if (c==ra) {
+               s=9;
+            } else {
+               s=8; doExpr(c);
+            }
+            break;
+         }
+         break;
+      case 9:
+         switch (c) {
+         case EOF: s=8; doExpr(ra); endExpr(); break;
+         default: 
+            if (c==ra) {
+               s=10; 
+            } else {
+               s=8; doExpr(ra); doExpr(c);
+            }
+            break;
+         }
+         break;
+      case 10:
+         switch (c) {
+         case EOF: s=8; doExpr(ra); doExpr(ra); endExpr(); break;
+         default: 
+            if (c==ra) {
+               s=5; endExpr(); begText();
+            } else {
+               s=8; doExpr(ra); doExpr(ra); doExpr(c);
+            }
+            break;
+         }
+         break;
+      } 
+      if (c==NL) {
+         lineno++;
+      }
+
+   } while (c!=EOF);
+
+   if (ferror(fi)) {
+      fprintf(stderr,"file read error (errno=%d: %s)\n", errno, strerror(errno));
+      exit(1);
+   }
+}
+
+void process_file(lua_State *L, const char* iname, const char* oname, int tflag, int mflag, int vflag, const char* pattern, int n, int cs, int cle, int cre) {
+
+   if (vflag) {
+      fprintf(stderr, "processing file '%s' -> '%s', (%d,'%c','%c','%c')\n", iname, oname, n,cs,cle,cre);
+   }
+
+   const char* tname;
+   FILE* fi;
+   FILE* ft;
+   struct stat tstats;
+   int isStdin = (strcmp(iname,"-")==0);
+   int isStdout = (strcmp(oname,"-")==0);
+
+   mflag = mflag || isStdin || isStdout;
+
+   if (isStdin) {
+      fi = stdin;
+   } else {
+      fi = fopen(iname,"r");
+      if (fi==NULL) {
+         fprintf(stderr,"can't open input file (file='%s', errno=%d: %s)\n", iname, errno, strerror(errno));
+         exit(1);
+      }
+   }
+
+   if (!isStdout) {
+      stdout = freopen(oname, "w", stdout);
+      if (stdout==NULL) {
+         fprintf(stderr,"can't open output file (file='%s', errno=%d: %s)\n", oname, errno, strerror(errno));
+         exit(1);
+      }
+   }
+
+   tname = tmpnam(NULL);
+   if (tname==NULL) {
+      fprintf(stderr,"can't obtain temporary file name (errno=%d: %s)\n", errno, strerror(errno));
+      exit(1);
+   }
+   ft = fopen(tname,"w");
+   if (ft==NULL) {
+      fprintf(stderr,"can't open temporary file (file='%s', errno=%d: %s)\n", tname, errno, strerror(errno));
+      exit(1);
+   }
+   convert(fi,ft,n,cs,cle,cre);
+   fclose(ft);
+
+   if (!isStdin) {
+      fclose(fi);
+   }
+
+   if (luaL_dofile(L,tname)!=0) {
+      fprintf(stderr,"%s (file='%s')\n",lua_tostring(L,-1),tname);
+      lua_pop(L,1);
+      exit(1);
+   }
+
+//   if (!isStdout) {
+//        stdout = freopen("/dev/null", "w", stdout);
+//        if (stdout==NULL) {
+//           fprintf(stderr,"can't open /dev/null (errno=%d: %s)\n", errno, strerror(errno));
+//           exit(1);
+//        }
+//   }
+
+   if (!mflag) {
+      if (stat(iname,&tstats) != 0) {
+         fprintf(stderr,"can't stat input file (file='%s', errno=%d: %s)\n", iname, errno, strerror(errno));
+         exit(1);
+      }
+      if (chmod(oname,tstats.st_mode)!=0) {
+         fprintf(stderr,"can't chmod output file (file='%s', mode=%o, errno=%d: %s)\n", tname, tstats.st_mode, errno, strerror(errno));
+         exit(1);
+      }
+   }
+
+   if (!tflag) {
+      if (unlink(tname)!=0) {
+         fprintf(stderr,"can't remove temporary file (file='%s', errno=%d: %s)\n", tname, errno, strerror(errno));
+         exit(1);
+      }
+   } else if (vflag){
+      fprintf(stderr,"temporary file '%s'\n",tname); 
+   }
+}
+
+void process_dir(lua_State *L, const char* dname, int tflag, int mflag, int vflag, const char* pattern, const char* replace, int level) {
+
+   if (vflag) {
+      fprintf(stderr, "processing dir(%d) '%s'\n", level, dname);
+   }
+
+   DIR *dirp = opendir(dname);
+   if (dirp==NULL) {
+      fprintf(stderr,"can't open directory (name='%s', errno=%d: %s)\n", dname, errno, strerror(errno));
+      exit(1);
+   }
+   struct dirent* dp;
+   while ((dp=readdir(dirp)) != NULL) {
+      switch(dp->d_type) {
+      case DT_REG:
+         lua_getglobal(L,"miconf_fname_hook");
+         lua_pushinteger(L,level);
+         lua_pushstring(L,pattern);
+         lua_pushstring(L,dname);
+         lua_pushstring(L,dp->d_name);
+         lua_pushinteger(L,dp->d_type);
+         lua_pushstring(L,replace);
+         if (lua_pcall(L, 6, 3, 0) != 0) {
+            fprintf(stderr,"error running lua function 'miconf_fname_hook': %s\n", lua_tostring(L,-1));
+            exit(1);
+         }
+         if (lua_istable(L,-1) && lua_isstring(L,-2) && lua_isstring(L,-3)) {
+            const char* iname = lua_tostring(L,-3);
+            const char* oname = lua_tostring(L,-2);
+
+            lua_pushinteger(L,1);
+            lua_gettable(L,-2);
+            int n = lua_tointeger(L,-1);
+            lua_pop(L,1);
+
+            lua_pushinteger(L,2);
+            lua_gettable(L,-2);
+            int cs = lua_tointeger(L,-1);
+            lua_pop(L,1);
+
+            lua_pushinteger(L,3);
+            lua_gettable(L,-2);
+            int cle = lua_tointeger(L,-1);
+            lua_pop(L,1);
+
+            lua_pushinteger(L,4);
+            lua_gettable(L,-2);
+            int cre = lua_tointeger(L,-1);
+            lua_pop(L,1);
+
+            process_file(L,iname,oname,tflag,mflag,vflag,pattern,n,cs,cle,cre);
+         }
+         lua_pop(L,3);  
+         break;
+      case DT_DIR:
+         if (strcmp(dp->d_name,".")==0 || strcmp(dp->d_name,"..")==0) 
+            break;
+         lua_getglobal(L,"miconf_dname_hook");
+         lua_pushinteger(L,level);
+         lua_pushstring(L,dname);
+         lua_pushstring(L,dp->d_name);
+         if (lua_pcall(L, 3, 1, 0) != 0) {
+            fprintf(stderr,"error running lua function 'miconf_dname_hook': %s\n", lua_tostring(L,-1));
+            exit(1);
+         }
+         if (lua_isstring(L,-1)) {
+            process_dir(L,lua_tostring(L,-1),tflag,mflag,vflag,pattern,replace,level+1);
+         }
+         lua_pop(L,1);
+         break;
+      }
+   }
+   closedir(dirp);
+
+}
+
+void version() {
+      fprintf(stderr,"Miconf: configuration utility\n");
+      fprintf(stderr,"Miconf %s Copyright (c) 2012 ikh software, inc.\n", miconf_VERSION );
+      fprintf(stderr,"%s\n", LUA_COPYRIGHT);
+}
+
+void usage() {
+      fprintf(stderr,"Miconf: configuration utility\n");
+      fprintf(stderr,"Miconf %s Copyright (c) 2012 ikh software, inc.\n", miconf_VERSION );
+      fprintf(stderr,"%s\n\n", LUA_COPYRIGHT);
+      fprintf(stderr,"Commit: %s\n\n", miconf_VERSION_RAW );
+      fprintf(stderr,"Usage:\n");
+      fprintf(stderr,"   miconf [options] template_file output_file\n");
+      fprintf(stderr,"   miconf [options] -r directory\n\n");
+      fprintf(stderr,"Options:\n");
+      fprintf(stderr,"   -c file -- config file, for example: -c config.lua \n");
+      fprintf(stderr,"   -e block -- config block, for example: -e 'host=\"foo\"; ip=\"127.0.0.1\"' \n");
+      fprintf(stderr,"   -p pattern -- template file name pattern (see 'miconf_fname_hook', default: '[.]template$')\n");
+      fprintf(stderr,"   -s replace -- file name pattern replacement (see 'miconf_fname_hook', default: '')\n");
+      fprintf(stderr,"   -t -- preserve temp files\n");
+      fprintf(stderr,"   -m -- disable chmod\n");
+      fprintf(stderr,"   -v -- verbose\n");
+      fprintf(stderr,"   -V -- version\n");
+      fprintf(stderr,"   -h -- help\n\n");
+}
+
+
+int main(int argc, char* argv[]) {
+   int ch;
+   int rflag = 0;
+   int tflag = 0;
+   int mflag = 0;
+   int vflag = 0;
+   const char* pattern = "[.]template$";
+   const char* replace = "";
+   lua_State *L = luaL_newstate();
+   luaL_openlibs(L);
+
+
+   const char* hooks = 
+      "function miconf_dname_hook(level,path,file)\n"
+      "   if file == \".git\" then\n"
+      "      return nil\n"
+      "   else\n"
+      "      return path..(file and (\"/\"..file) or \"\")\n"
+      "   end\n"
+      "end\n"
+      "function miconf_markup_hook()\n"
+      "   return {3,string.byte(\"=\"),string.byte(\"<\"),string.byte(\">\")}\n"
+      "end\n"
+      "function miconf_fname_hook(level,pattern,path,file,type,replace)\n"
+      "   ofile,cnt = file:gsub(pattern,replace,1)\n"
+      "   if ofile and cnt==1 and ofile:len()>0 then\n"
+      "      return path..(file and (\"/\"..file) or \"\"), path..\"/\"..ofile, miconf_markup_hook()\n"
+      "   else\n"
+      "      return nil,nil,nil\n"
+      "   end\n"
+      "end\n"
+   ;
+
+   if (luaL_dostring(L,hooks)!=0) {
+      fprintf(stderr,"%s (string='%s')\n",lua_tostring(L,-1),hooks);
+      lua_pop(L,1);
+      exit(1);
+   }
+
+   while ((ch = getopt(argc, argv, "hVrtmvp:s:e:c:")) != -1) {
+      switch (ch) {
+      case 'V':
+         version();
+         exit(0);
+      case 'h':
+         usage();
+         fprintf(stderr,"Default config:\n\n");
+         fprintf(stderr,"%s\n",hooks);
+         fprintf(stderr,"----\n\n");
+         exit(0);
+      case 'r': rflag = 1; break;
+      case 't': tflag = 1; break;
+      case 'm': mflag = 1; break;
+      case 'v': vflag = 1; break;
+      case 'p': pattern = optarg; break;
+      case 's': replace = optarg; break;
+      case 'c':
+         if (luaL_dofile(L,optarg)!=0) {
+            fprintf(stderr,"%s (file='%s')\n",lua_tostring(L,-1),optarg);
+            lua_pop(L,1);
+            exit(1);
+         }
+         break;
+      case 'e':
+         if (luaL_dostring(L,optarg)!=0) {
+            fprintf(stderr,"%s (block='%s')\n",lua_tostring(L,-1),optarg);
+            lua_pop(L,1);
+            exit(1);
+         }
+         break;
+      default:
+         usage();
+         exit(1);
+      }
+   }
+   argc -= optind;
+   argv += optind;
+
+   if (!rflag) {
+      const char* iname = argc>0 ? argv[0] : "-";
+      const char* oname = argc>1 ? argv[1] : "-";
+      lua_getglobal(L,"miconf_markup_hook");
+      if (lua_pcall(L, 0, 1, 0) != 0) {
+         fprintf(stderr,"error running lua function 'miconf_markup_hook': %s\n", lua_tostring(L,-1));
+         exit(1);
+      }
+
+      if (!lua_istable(L,-1)) {
+         fprintf(stderr,"'miconf_markup_hook' must return a table\n");
+         exit(1);
+      }
+
+      lua_pushinteger(L,1);
+      lua_gettable(L,-2);
+      int n = lua_tointeger(L,-1);
+      lua_pop(L,1);
+
+      lua_pushinteger(L,2);
+      lua_gettable(L,-2);
+      int cs = lua_tointeger(L,-1);
+      lua_pop(L,1);
+
+      lua_pushinteger(L,3);
+      lua_gettable(L,-2);
+      int cle = lua_tointeger(L,-1);
+      lua_pop(L,1);
+
+      lua_pushinteger(L,4);
+      lua_gettable(L,-2);
+      int cre = lua_tointeger(L,-1);
+      lua_pop(L,1);
+
+      process_file(L,iname,oname,tflag,mflag,vflag,pattern,n,cs,cle,cre);
+
+      lua_pop(L,1);
+   } else {
+      const char* dname = argc>0 ? argv[0] : ".";
+      lua_getglobal(L,"miconf_dname_hook");
+      lua_pushinteger(L,0);
+      lua_pushstring(L,dname);
+      lua_pushnil(L);
+      if (lua_pcall(L, 3, 1, 0) != 0) {
+         fprintf(stderr,"error running lua function 'miconf_dname_hook': %s\n", lua_tostring(L,-1));
+         exit(1);
+      }
+      if (lua_isstring(L,-1)) {
+         process_dir(L,lua_tostring(L,-1),tflag,mflag,vflag,pattern,replace,1);
+      }
+      lua_pop(L,1);
+   }
+
+   lua_close(L);
+
+   return(0);
+}
+
---
+++ piconf/piconf.md
@@ -0,0 +1,112 @@
+#miconf
+##Name
+`miconf` - lightweight configuration utility  
+##Synopsis
+	miconf -h
+	miconf [options] -r directory
+	miconf [options] template_file output_file
+##Description
+`miconf` is a lightweight configuration utility based on simple template substitution technique and [Lua](http://www.lua.org/) expressions. It executes a configuration file (a Lua program), and then uses the results of the execution (its global state) in template substitution. It reads `template_file` and substitutes `<<<`*lua expression*`>>>` placeholders with evaluated *lua expression*. It also executes lines that start with `===`. 
+
+For example:
+
+	. . .
+	=== for x = 1, 3 do
+	<<<x>>> ^ 3 = <<< x^3 >>>
+	=== end
+	. . .
+
+will result in the following output:
+
+	. . .
+	1 ^ 3 = 1
+	2 ^ 3 = 8
+	3 ^ 3 = 27
+	. . .
+
+		
+When `miconf` is run in recoursive `-r` mode, it traverses `directory` recoursively and processes files based on the pattern (`-p` option) and the output of callback functions.
+
+`miconf` executable is a self sufficient executable that requires only a C runtime.
+      
+##Options
+
+| Option 	| Default value 	| Description 	| Example 	| 
+| :--- 	| :--- 	| :--- 	| :--- 	|
+| `-c file` 	| `'config.lua'` 	| config file 	| `-c system.config` 	|
+| `-e block` 	| `''` 	| config block 	| `-e 'host="foo"; ip="127.0.0.1"'`	|
+| `-p pattern` 	| `'[.]template$'` 	| template file name pattern (see `miconf_fname_hook`) 	| `-p '[.]miconf([.]?)'` 	|
+| `-s replace` 	| `''` 	| file name pattern replacement (see `miconf_fname_hook`) 	| `-s '%1'` 	|
+| `-t` 	| 	| preserve temp files 	| 	|
+| `-m` 	| 	| disable chmod 	| 	|
+| `-v` 	| 	| verbose 	| 	|
+
+##Default hooks
+
+The following "hooks" are used if you do not redefine them in your configuration files. 
+These functions get called when `miconf` is run in recoursive mode using `-r` option. `miconf_dname_hook` is invoked for each subdirectory, and `miconf_fname_hook` is called for each regular file. `miconf_dname_hook` has to return a full path to the subdirectory to traverse, or `nil` to ignore the whole subdirectory. `miconf_fname_hook` has to return input file path, output file path and markup syntax description table (see `miconf_markup_hook` below), or `(nil,nil,nil)` if you want to ignore the file. These functions allow for customization of miconf's behavior, i.e. what parts of your tree and what files to process, and how to come up with output file names. For example, you may keep your templates at a separate directory and output files into your output tree, etc. You may adjust miconf's markup syntax with `miconf_markup_hook`.
+
+	function miconf_dname_hook(level,path,file)
+	   return path..(file and ("/"..file) or "")
+	end
+	
+	function miconf_markup_hook()
+	   return {3,string.byte("="),string.byte("<"),string.byte(">")}
+	end
+
+	function miconf_fname_hook(level,pattern,path,file,type,replace)
+	   ofile,cnt = file:gsub(pattern,replace,1)
+	   if ofile and cnt==1 and ofile:len()>0 then
+	      return path..(file and ("/"..file) or ""), path.."/"..ofile, miconf_markup_hook()
+	   else
+	      return nil,nil,nil
+	   end
+	end
+
+
+##Example
+
+**`$ cat sample.config`**
+
+	a = a or 5
+	b = 200
+	c = "Hello, world!"
+	if a < 100 then
+	   d = {x=a, "boo"}
+	else
+	   d = {x=10, "foo"}
+	end
+	
+	function square(x)
+	   return x*x
+	end
+
+
+**`$ cat sample.template`**
+
+	text0
+	text1,<<<c>>>,text2
+	=== if b > 100 then
+	text3
+	=== end
+	text4
+	=== for i = 1, a do
+	text5,<<<square(i)>>>,text6,<<<d[1]>>>,text7
+	=== end
+	text8
+
+**`$ miconf -v -e 'a=5' -c sample.config sample.template sample`**
+	
+**`$ cat sample`**
+
+	text0
+	text1,Hello, world!,text2
+	text3
+	text4
+	text5,1,text6,boo,text7
+	text5,4,text6,boo,text7
+	text5,9,text6,boo,text7
+	text5,16,text6,boo,text7
+	text5,25,text6,boo,text7
+	text8
+
